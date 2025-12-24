f3bbdb343c6fccfc05d938a079fed86e8a969764
bishal-dd <dhakalbishal930@gmail.com>
2025-12-24 18:29:59 +0600

    update authClient credentials include

--- src/shared/helper/auth/authClient.ts
+++ src/shared/helper/auth/authClient.ts
@@ -2,4 +2,5 @@ import { createAuthClient } from "better-auth/client";
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  credentials: "include",
 });
