824864d0a3518db6b4b0698564685bb5a3ea3843
Choedra <choedra999@gmail.com>
2026-04-03 11:13:18 +0600

    fix eslint

--- src/shared/session/useSession.ts
+++ src/shared/session/useSession.ts
@@ -2,14 +2,12 @@
 
 import { useSessionStore } from "./sessionStore";
 import { authClient } from "@/shared/helper/auth/authClient";
-import { useRouter } from "next/navigation";
 
 export function useSession() {
   const session = useSessionStore((s) => s.session);
   const loading = useSessionStore((s) => s.loading);
   const setSession = useSessionStore((s) => s.setSession);
   const fetchSession = useSessionStore((s) => s.fetchSession);
-  const router = useRouter();
 
   const signOut = async () => {
     await authClient.signOut();
